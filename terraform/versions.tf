7f6261c47ea5e28184f4c54df99ab6416fb4d53c
ido-hail <ido16h@gmail.com>
2026-08-11 14:48:09 +0300

    configure terraform remote state backend

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -7,4 +7,12 @@ terraform {
       version = "~> 6.55"
     }
   }
+
+  backend "s3" {
+    bucket       = "pacman-terraform-state-506456084249-us-east-1"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
